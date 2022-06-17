b016ba05c20be34d5963e4376de50d75293c5295
ItsRayRay <r.boerema21@gmail.com>
2022-06-17 11:30:21 +0200

    add page sections

--- src/index.js
+++ src/index.js
@@ -2,13 +2,74 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
 
-function component() {
-    const element = document.createElement('div');
+// function component() {
+//     const element = document.createElement('div');
   
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    return element;
-  }
+//     // Lodash, now imported by this script
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     element.classList.add('hello');
+//     return element;
+//   }
+
+// function render() {
+//     document.body.appendChild(component());
+// }
+//   document.body.appendChild(component());
+
+
+//======================HEADER ELEMENTS============================//
+  const header = document.createElement('header')
+  const homeBtn = document.createElement('button')
+  const aboutBtn = document.createElement('button')
+  const contactBtn = document.createElement('button')
+
+
+//======================SECTION 1 ELEMENTS============================//
+
+
+  const firstSection = document.createElement('div')
+  const firstSectionWrapper = document.createElement('div')
+  const firstSectionTitle = document.createElement('h1')
+  const firstSectionText = document.createElement('p')
+  const firstSectionImage = document.createElement('img')
+
+
+//======================SECTION 2 ELEMENTS============================//
+
+
+  const secondSection = document.createElement('div')
+  const secondSectionWrapper = document.createElement('div')
+  const secondSectionTitle = document.createElement('h1')
+  const secondSectionText = document.createElement('p')
+
+
+//======================SECTION 3 ELEMENTS============================//
+
   
-  document.body.appendChild(component());
+  const thirdSection = document.createElement('div')
+  const thirdSectionWrapper = document.createElement('div')
+  const thirdSectionTitle = document.createElement('h1')
+  const thirdSectionText = document.createElement('p')
+  const thirdSectionImage = document.createElement('img')
+
+
+
+
+
+
+
+  // const div = document.createElement('div');
+  // const footer = document.createElement('footer')
+  // const container = document.createElement('div').classList.add('container')
+  // const wrapper = document.createElement('div').classList.add('wrapper')
+
+
+
+
+
+  // div.appendChild(header)
+  
+
+
+
+  // body.appendChild(container)
